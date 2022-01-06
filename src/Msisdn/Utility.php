b26fb3e47150cae1ad403f87c28b84641fd6c8c5
allan kibet <allan.koskei@gmail.com>
2022-01-06 11:42:26 +0300

    Refactor v1.0.1

--- src/Msisdn/Utility.php
+++ src/Msisdn/Utility.php
@@ -6,29 +6,47 @@ namespace Msisdn;
  * MSISDN Utility class
  * @author Allan Kibet <allan.koskei@gmail.com>
  * @see https://en.wikipedia.org/wiki/Telephone_numbers_in_Kenya
- * @version 1.0.0
+ * @version 1.0.1
  */
-class Utility
+class Utility 
 {
+    const MNO_SAFARICOM = 'Safaricom';
+    const MNO_AIRTEL = "Airtel";
+    const MNO_EQUITEL = "Equitel";
+    const MNO_FAIBA = "Faiba";
+    const MNO_TELKOM = "Telkom";
+
+
     /**
-     * Standardize MSISDN Format
+     * Standardize MSISDN Format. Returns -1 if MSISDN is invalid
      *
      * @param int $msisdn
      * @return int
      */
     public static function clean(string $msisdn) : int
     {
+        // Remove any white space and (+) character in the string
+        $msisdn = str_replace('+', '', preg_replace("/\s+/", "", $msisdn));
+
         if (substr($msisdn, 0, 1) == '0') {
             $msisdn = '254' . ltrim($msisdn, '0');
         }
+
         if (substr($msisdn, 0, 1) == '7') {
             $msisdn = '254' . $msisdn;
         }
+
+        // Allowance for new 25411... extensions
+        if (substr($msisdn, 0, 1) == '1') {
+            $msisdn = '254' . $msisdn;
+        }
+        
         return self::checkMSISDNLength(intval($msisdn));
     }
 
+
     /**
-     * Check the message count
+     * Check the MSISDN length
      *
      * @param int $msisdn
      * @return int
@@ -38,54 +56,49 @@ class Utility
         return (int)(strlen($msisdn) == 12 && is_numeric($msisdn)) ? $msisdn : -1;
     }
 
+
     /**
-     * Get telco channel associated with the number
+     * Get Mobile Network Operator channel associated for a given phone number
      *
      * @param string $msisdn
-     * @return string
+     * @return any
      */
-    public static function channel(string $msisdn) : string
+    public static function getMobileNetworkOperator(string $msisdn): ?string
     {
-        $clean_msisdn = self::clean($msisdn);
+        $cleanMsisdn = self::clean($msisdn);
 
-        if (in_array(substr($clean_msisdn, 0, 6), Utility::SAFARICOM_PREFIXES)) {
-            return strval('SAFARICOM');
-        } 
+        $kenyanMNOs = [
+            self::MNO_SAFARICOM => self::fetchPrefixesByMNO(self::MNO_SAFARICOM),
+            self::MNO_AIRTEL => self::fetchPrefixesByMNO(self::MNO_AIRTEL),
+            self::MNO_EQUITEL => self::fetchPrefixesByMNO(self::MNO_EQUITEL),
+            self::MNO_TELKOM => self::fetchPrefixesByMNO(self::MNO_TELKOM),
+            self::MNO_FAIBA => self::fetchPrefixesByMNO(self::MNO_FAIBA)
+        ];
 
-        if (in_array(substr($clean_msisdn, 0, 6), Utility::AIRTEL_PREFIXES)) {
-            return strval('AIRTEL');
-        } 
+        foreach ($kenyanMNOs as $k => $v) {
+            if (in_array(substr($cleanMsisdn, 0, 6), $v) || in_array(substr($cleanMsisdn, 0, 5), $v)) {
+                return $k;
+            } 
+        }
 
-        if (in_array(substr($clean_msisdn, 0, 6), Utility::TELKOM_PREFIXES)) {
-            return strval('TELKOM');
-        } 
-
-        if (in_array(substr($clean_msisdn, 0, 6), Utility::EQUITEL_PREFIXES)) {
-            return strval('EQUITEL');
-        } 
-        if (in_array(substr($clean_msisdn, 0, 6), Utility::FAIBA_PREFIXES)) {
-            return strval('FAIBA');
-        } 
-
-        return strval('UNDEFINED');
+        return null;
     }
 
-    const SAFARICOM_PREFIXES = [
-        "254701","254702","254703","254704","254705","254706","254707",
-        "254708","254710","254711","254712","254713","254714","254715",
-        "254716","254717","254718","254719","254720", "254721", "254722",
-        "254723", "254724", "254725","254726","254727","254728","254729",
-        "254790", "254791","254792"];
 
-    const AIRTEL_PREFIXES = ["254730","254731","254732","254733","254734",
-        "254735","254736", "254737","254738","254739","254750","254751","254752",
-        "254753","254754","254755","254756","254785","254786","254787","254788",
-        "254789"];
+    /**
+     * Fetch prefixes from JSON files
+     *
+     * @param string $mno
+     * @return array
+     */
+    private static function fetchPrefixesByMNO(string $mno) : ?array
+    {
+        $data = @file_get_contents("prefixes/". strtolower($mno). ".json");
 
-    const TELKOM_PREFIXES = ["254770","254771","254772","254773","254774","254775","254776"];
+        if ($data === false) {
+            return null;
+        }
 
-    const EQUITEL_PREFIXES = ["254763","254764","254765"];
-
-    const FAIBA_PREFIXES = ["254747"];
-
+        return json_decode($data);      
+    }
 }
